8a42d4fe9b9fbbfd791cde54edb029addce674dd
thomasguillot <thomasguillot@d957f892-c61d-0410-b221-f235e6eecf30>
2014-04-01 09:28:28 +0000

    Illustratr: Wrap sharedaddy in a div and move it after entry-header for aside, quote and status post format

--- illustratr/js/illustratr.js
+++ illustratr/js/illustratr.js
@@ -4,15 +4,15 @@
 	 * Add classes to images and captions if image post format.
 	 */
 	function image_post_format() {
-		
+
 		$( '.hentry.format-image img, .portfolio-entry .entry-content img' ).each( function() {
 			var img = $( this ),
 			    caption = $( this ).closest( 'div' ),
 			    new_img = new Image();
-			    
+
 			new_img.src = img.attr('src');
 			var img_width = new_img.width;
-			    
+
 			if ( img_width >= 1100 ) {
 				$( this ).addClass( 'image-big' );
 				$( this ).parents( 'p' ).addClass( 'image-big-wrapper' );
@@ -22,27 +22,27 @@
 				caption.addClass( 'caption-big' );
 			}
 		} );
-		
+
 	}
-	
+
 	/*
 	 * Remove content if empty.
 	 */
 	function content() {
-		
+
 		$( '.page-content, .entry-content, .entry-summary' ).each( function() {
 			if ( $.trim( $( this ).text() ) === '' ) {
 				$( this ).remove();
 			}
 		} );
-		
+
 	}
-	
+
 	/*
 	 * Resize entry-thumbnail, entry-gallery, entry-media, video-wrapper (video post format), caption-big and image-big for full width on small screens.
 	 */
 	function calc() {
-		
+
 		$( '.entry-thumbnail, .entry-gallery, .entry-media, .hentry.format-video > .video-wrapper, .caption-big' ).each( function() {
 			if( $( window ).width() < 960 ) {
 	    		$( this ).css( {
@@ -78,28 +78,43 @@
 				} );
 			}
 		} );
-				
+
 	}
-	
+
+	/*
+	 * Wrap sharedaddy in a div and move it after entry-header for aside, quote and status post format.
+	 */
+	function sharedaddy() {
+
+		$( '.hentry.format-aside .entry-content > .sharedaddy, .hentry.format-quote .entry-content > .sharedaddy, .hentry.format-status .entry-content > .sharedaddy' ).each( function() {
+			$( this ).wrap( '<div class="entry-sharedaddy" />' );
+		} );
+		$( '.entry-sharedaddy' ).each( function() {
+			$( this ).insertAfter( $( this ).closest( '.hentry' ).find( '.entry-header' ) );
+		} );
+
+	}
+
 	/*
 	 * Load all the functions.
 	 */
 	function load_fuctions() {
-	
+
 		image_post_format();
 		content();
 		calc();
-		
+		sharedaddy();
+
 	}
 	$( window ).load( function() {
-		
+
 		load_fuctions();
-		
+
 		// Show content
 		$( '#primary' ).animate( {
 			'opacity': 1
 		}, 250 );
-		
+
 	} ).resize( _.debounce( load_fuctions, 100 ) );
 	$( document ).on( 'post-load', load_fuctions );
 
