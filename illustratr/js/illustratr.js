5d1584708cb77cb268d77ffc04753a071df5f1de
sixhours <sixhours@d957f892-c61d-0410-b221-f235e6eecf30>
2015-03-03 17:48:52 +0000

    Illustratr: Check for the presence of an iFrame in the .entry-content selector before removing that selector; this was preventing iFrames, like Google Calendar, from appearing on their own in the content area. Fixes #2978

--- illustratr/js/illustratr.js
+++ illustratr/js/illustratr.js
@@ -67,7 +67,7 @@
 	function content() {
 
 		$( '.page-content, .hentry:not(.portfolio-entry) .entry-content, .entry-summary, .entry-sharedaddy, .sharedaddy' ).each( function() {
-			if ( ! $( this ).find( 'img' ).length && $.trim( $( this ).text() ) === '' ) {
+			if ( ! $( this ).find( 'iframe' ).length && ! $( this ).find( 'img' ).length && $.trim( $( this ).text() ) === '' ) {
 				$( this ).remove();
 			}
 		} );
